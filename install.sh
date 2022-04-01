def65bd6bd3eed10a6ed3780cf43af5af3ad6bd4
Selena Latchman <selena.latchman@shopify.com>
2022-04-01 11:19:59 -0400

    Rename zshrc file to .zshrc and don't put /Users/selenalatchman/.oh-my-zsh in quotes

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # link zshrc to be correct directory
-ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
